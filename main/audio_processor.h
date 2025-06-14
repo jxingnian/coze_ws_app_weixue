03e6e92a1b6364f4fbf77043744e667a33665fea
xingnian <j_xingnian@163.com>
2025-06-14 23:10:53 +0800

    音频加到90

--- main/audio_processor.h
+++ main/audio_processor.h
@@ -11,147 +11,147 @@ extern "C" {
 #endif  /* __cplusplus */
 
 /**
- * @brief  Type definition for the audio recorder event callback function
+ * @brief  音频录制事件回调函数的类型定义
  *
- * @param[in]  event  Pointer to the event data, which can contain information about the recorder's state
- * @param[in]  ctx    User-defined context pointer, passed when registering the callback
+ * @param[in]  event  指向事件数据的指针，可以包含有关录音机状态的信息
+ * @param[in]  ctx    用户定义的上下文指针，在注册回调时传递
  */
 typedef void (*recorder_event_callback_t)(void *event, void *ctx);
 
 /**
- * @brief  Initializes the audio manager module.
+ * @brief  初始化音频管理器模块
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_manager_init();
 
 /**
- * @brief  Deinitializes the audio manager component
+ * @brief  反初始化音频管理器组件
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_manager_deinit();
 
 /**
- * @brief  Opens the audio recorder and registers an event callback
+ * @brief  打开音频录制器并注册事件回调
  *
- * @param[in]  cb   Pointer to the event callback function, which will be invoked on recorder events
- * @param[in]  ctx  User-defined context pointer passed to the callback function
+ * @param[in]  cb   指向事件回调函数的指针，将在录音机事件上调用
+ * @param[in]  ctx  传递给回调函数的用户定义上下文指针
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_recorder_open(recorder_event_callback_t cb, void *ctx);
 
 /**
- * @brief  Closes the audio recorder
+ * @brief  关闭音频录制器
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_recorder_close(void);
 
 /**
- * @brief  Reads audio data from the recorder.
+ * @brief  从录音机读取音频数据
  *
- * @param[out]  data       Pointer to the buffer where the audio data will be stored
- * @param[in]   data_size  Size of the buffer to store the audio data.
+ * @param[out]  data       指向将存储音频数据的缓冲区的指针
+ * @param[in]   data_size  存储音频数据的缓冲区大小
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_recorder_read_data(uint8_t *data, int data_size);
 
 /**
- * @brief  Opens the audio playback system
+ * @brief  打开音频播放系统
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_playback_open();
 
 /**
- * @brief  Closes the audio playback module
+ * @brief  关闭音频播放模块
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_playback_close(void);
 
 /**
- * @brief  Starts the audio playback operation.
+ * @brief  启动音频播放操作
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_playback_run(void);
 
 /**
- * @brief  Stops the ongoing audio playback
+ * @brief  停止正在进行的音频播放
  *
  * @return
- *       - ESP_OK  On successfully stopping the audio playback
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功停止音频播放
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_playback_stop(void);
 
 /**
- * @brief  Feeds audio data into the playback system
+ * @brief  向播放系统提供音频数据
  *
- * @param[in]  data       Pointer to the audio data to be fed into the playback system
- * @param[in]  data_size  Size of the audio data to be played
+ * @param[in]  data       指向要输入到播放系统的音频数据的指针
+ * @param[in]  data_size  要播放的音频数据的大小
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_playback_feed_data(uint8_t *data, int data_size);
 
 /**
- * @brief  Opens the audio prompt system
+ * @brief  打开音频提示系统
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_prompt_open(void);
 
 /**
- * @brief  Closes the audio prompt functionality
+ * @brief  关闭音频提示功能
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_prompt_close(void);
 
 /**
- * @brief  Plays an audio prompt from the specified URL
+ * @brief  从指定URL播放音频提示
  *
- * @param[in]  url  URL pointing to the audio prompt file to be played
+ * @param[in]  url  指向要播放的音频提示文件的URL
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_prompt_play(const char *url);
 
 /**
- * @brief  Stops the currently playing audio prompt
+ * @brief  停止当前播放的音频提示
  *
  * @return
- *       - ESP_OK  On success
- *       - Other   Appropriate esp_err_t error code on failure
+ *       - ESP_OK  成功
+ *       - Other   失败时返回相应的esp_err_t错误代码
  */
 esp_err_t audio_prompt_stop(void);
 
